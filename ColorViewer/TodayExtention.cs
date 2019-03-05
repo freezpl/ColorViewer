dfdcd486161f60657b2a823f2c40bfa837a55fb6
freezpl <freezpl@gmail.com>
2019-03-05 16:34:33 +0200

    Local Extention Created

---
+++ ColorViewer/TodayExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace ColorViewer
+{
+    internal sealed class TodayExtention : MarkupExtension
+    {
+        private int shift;
+
+        public TodayExtention() : this(0){}
+
+        public TodayExtention(int shift)
+        {
+            this.shift = shift;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return DateTime.Now.AddDays(shift).DayOfWeek.ToString();
+        }
+    }
+}
